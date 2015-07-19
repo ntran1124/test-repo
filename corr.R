f3f0f6f6b1653a4db15d713e3a68efd13d4ab715
ntran1124 <ntran1124@gmail.com>
2015-07-19 11:59:29 -0700

    Commit 3 header-only .R files

---
+++ corr.R
@@ -0,0 +1,7 @@
+corr <- function(directory, threshold = 0) {
+        correlation <- vector(mode="numeric", length=0) ## create an empty numeric vector for correlations
+        
+        ## your code will be here
+
+        return(correlation)
+}
---
+++ pollutantmean.R
@@ -0,0 +1,4 @@
+pollutantmean <- function(directory, pollutant, id = 1:332) {
+        ## your code will be replacing the 0 return value
+        0
+}
